d83ba6beb8b79ee319b36a5b38e18dec7dd896c9
kale-stew <kylieastewart@gmail.com>
2017-10-25 00:45:53 -0700

    built a new user form

--- src/scenes/SignUp.js
+++ src/scenes/SignUp.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { View } from 'react-native';
+import { Card, CardSection, Input, Button, Header } from '../components/common';
 
-const SignUp = () => {
-
+class SignUp extends React.Component {
+    render() {
+        return(
+            <View style={{ paddingTop: 60, marginLeft: 20, marginRight: 20 }}>
+                <Card>
+                    <CardSection><Input label="First Name" placeholderText="Kylie"/></CardSection>
+                    <CardSection><Input label="Last Name" placeholderText="Stewart"/></CardSection>
+                    <CardSection><Input label="Email" placeholderText="your_email@gmail.com"/></CardSection>
+                    <CardSection><Input label="Password" placeholderText="password" secureTextEntry /></CardSection>
+                </Card>
+                <Card>
+                    <CardSection><Button>Click me</Button></CardSection>
+                </Card>
+            </View>
+        )
+    }
 };
 
 export default SignUp;

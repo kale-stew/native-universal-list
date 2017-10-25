595f32702e3d4f27c6c5f4ef65e2eb3cf665ee5c
kale-stew <kylieastewart@gmail.com>
2017-10-24 22:52:48 -0700

    initiate nodemon

--- src/server/index.js
+++ src/server/index.js
@@ -10,3 +10,12 @@ const express = require('express'),
 const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
+
+// massive(/*CONNECTION_STRING*/).then( db => {
+//     app.set('db', db);
+// }).catch( (err) => console.log(err) );
+
+// item_controller calls
+
+const PORT = 3030;
+app.listen( PORT, () => console.log(`Listening closely to ${PORT}`) );
